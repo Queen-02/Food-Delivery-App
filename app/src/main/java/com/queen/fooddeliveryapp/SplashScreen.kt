4e2b944803cdd2f5acafbebfe32d39aebb5064c6
Queen-02 <queenzyblack176@gmail.com>
2022-05-26 13:37:06 +0300

    created a teaser activity

--- app/src/main/java/com/queen/fooddeliveryapp/SplashScreen.kt
+++ app/src/main/java/com/queen/fooddeliveryapp/SplashScreen.kt
@@ -1,11 +1,15 @@
 package com.queen.fooddeliveryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+
+        val intent = Intent(this, TeaserActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
